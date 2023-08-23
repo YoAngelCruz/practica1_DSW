e5dcb72d4fdc5b1dcaa9eb373cf3f3660d848fb4
angel <angelcruz.feb@gmail.com>
2023-08-23 09:56:12 -0600

    ya cuaja bien

--- initdb.sql
+++ initdb.sql
@@ -1,11 +1,15 @@
 CREATE USER myuser;
 CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+ALTER DATABASE mydb OWNER TO myuser;
 
-CREATE TABLE IF NOT EXISTS pejemplo
+\connect mydb
+CREATE TABLE pejemplo
 (
 	clave integer NOT NULL,
 	nombre character varying,
 	direccion character varying,
 	CONSTRAINT pk_clave PRIMARY KEY (clave)
 );
+
+ALTER TABLE pejemplo OWNER TO myuser;
